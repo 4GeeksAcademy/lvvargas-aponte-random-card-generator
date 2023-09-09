b592f53e7aa5e70f2bddaf8056378e780ed75a24
Valeria V <54474237+lvvargas-aponte@users.noreply.github.com>
2023-09-09 16:17:17 +0000

    project completed

--- src/app.js
+++ src/app.js
@@ -2,10 +2,83 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const numberElement = document.querySelector("#randomCardNum");
+  const topSpadeElement = document.querySelector("#randomCardSpadeTop");
+  const bottomSpadeElement = document.querySelector("#randomCardSpadeBottom");
+
+  const getRandomElement = array => {
+    return array[Math.floor(Math.random() * array.length)];
+  };
+
+  const suits = ["♠", "♣", "♥", "♦"];
+  const cardNum = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const cardColor = ["red", "black"];
+
+  let randomNum = getRandomElement(cardNum);
+  let randomSuits = getRandomElement(suits);
+  let randomColor = getRandomElement(cardColor);
+
+  numberElement.innerHTML = randomNum;
+  topSpadeElement.innerHTML = randomSuits;
+  bottomSpadeElement.innerHTML = randomSuits;
+
+  if (randomColor != "black") {
+    numberElement.style.color = randomColor;
+    topSpadeElement.style.color = randomColor;
+    bottomSpadeElement.style.color = randomColor;
+  } else {
+    numberElement.style.color = randomColor;
+    topSpadeElement.style.color = randomColor;
+    bottomSpadeElement.style.color = randomColor;
+  }
+
+  const cardBody = document.querySelector("#cardBody");
+  const heightInput = document.querySelector("#heightInput");
+  const widthInput = document.querySelector("#widthInput");
+
+  const updateCardSize = () => {
+    let newHeight = heightInput.value;
+    let newWidth = widthInput.value;
+
+    newHeight = parseFloat(newHeight);
+    newWidth = parseFloat(newWidth);
+
+    if (isNaN(newHeight)) {
+      cardBody.style.height = "400px";
+    } else {
+      cardBody.style.height = `${newHeight}px`;
+    }
+
+    if (isNaN(newWidth)) {
+      cardBody.style.width = "325px";
+    } else {
+      cardBody.style.width = `${newWidth}px`;
+    }
+  };
+
+  const cardSizeForm = document.querySelector("#cardSizeForm");
+  cardSizeForm.addEventListener("input", updateCardSize);
+
+  const reloadEveryTenSec = () => {
+    return setTimeout(function() {
+      location.reload(true);
+    }, 10000);
+  };
+
+  const startTenSecReload = document.querySelector("#tenSecButton");
+  const stopTenSecReload = document.querySelector("#stopTenSecButton");
+  let timeoutId;
+
+  startTenSecReload.onclick = function() {
+    clearTimeout(timeoutId);
+    timeoutId = reloadEveryTenSec();
+  };
+
+  stopTenSecReload.onclick = function() {
+    clearTimeout(timeoutId);
+  };
 };
